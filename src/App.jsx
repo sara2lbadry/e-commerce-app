14a462984674e1b28315357bbfcc8ab2f1a1fb1a
Sara El Badry <sara2lbadry@gmail.com>
2022-09-23 19:03:21 +0200

    adjusting arrows with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Announcement from './components/Announcement'
 import Navbar from './components/Navbar'
+import Slider from './components/Slider'
 function App() {
   return (
     <div>
         <Announcement />
         <Navbar />
+        <Slider />
 
     </div>
   )
