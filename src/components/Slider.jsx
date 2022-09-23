8a5f301bfe82ade7ea331544293e16346798c072
Sara El Badry <sara2lbadry@gmail.com>
2022-09-23 19:26:05 +0200

    img&info container

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import { KeyboardArrowLeftOutlined, KeyboardArrowRightOutlined } from "@mui/icons-material"
 import styled from "styled-components"
+import Shopping from '../assets/shopping.png'
 
 const Container = styled.div`
     width: 100%;
@@ -21,8 +22,38 @@ const Arrow = styled.div`
     bottom: 0%;
     left: ${props => props.direction === 'left' && '10px'};
     right: ${props => props.direction === 'right' && '10px'};
-
     margin: auto;
+    cursor: pointer;
+    opacity: 0.7;
+`
+const Wrapper = styled.div`
+    height: 100%;
+`
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+    height: 100%;
+`
+const Image = styled.img`
+    height: 80%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    
+`
+const Description = styled.p`
+    
+`
+const Button = styled.button`
+    
 `
 
 function Slider() {
@@ -31,6 +62,18 @@ function Slider() {
         <Arrow direction = 'left'>
             <KeyboardArrowLeftOutlined />
         </Arrow>
+        <Wrapper>
+            <Slide>
+                <ImgContainer>
+                    <Image src={Shopping}/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Summer Sale</Title>
+                    <Description>Get 30% OFF For New Arrivals.</Description>
+                    <Button>Shop Now</Button>
+                </InfoContainer>
+            </Slide>
+        </Wrapper>
         <Arrow direction = 'right'>
             <KeyboardArrowRightOutlined />
         </Arrow>
