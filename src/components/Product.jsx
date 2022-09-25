73a3eb6f8c289c657703ae4e4f9f75973274e7fd
Sara El Badry <sara2lbadry@gmail.com>
2022-09-25 15:40:25 +0200

    product list hover & animation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,21 @@
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material"
 import styled from "styled-components"
 
+const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 3;
+    transition: all 0.5s ease;
+    cursor: pointer;
+`
 const Container = styled.div`
     flex: 1;
     margin: 5px;
@@ -11,6 +26,11 @@ const Container = styled.div`
     justify-content: center;
     background-color: #f5fbfd;
     position: relative;
+
+    &:hover ${Info}{
+        opacity: 1;
+
+    }
 `
 const Circle = styled.div`
     width: 200px;
@@ -23,18 +43,7 @@ const Image = styled.img`
     height: 75%;
     z-index: 2;
 `
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0%;
-    left: 0%;
-    background-color: gray;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 3;
-`
+
 const Icon = styled.div`
     width: 40px;
     height: 40px;
@@ -44,6 +53,12 @@ const Icon = styled.div`
     align-items: center;
     justify-content: center;
     margin: 10px;
+    transition: all 0.5s ease;
+
+    &:hover {
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+    }
 `
 function Product({item}) {
   return (
