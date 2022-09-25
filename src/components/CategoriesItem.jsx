13a813fd95be81ca0fec7029f59ec7eae7604cd2
Sara El Badry <sara2lbadry@gmail.com>
2022-09-25 15:01:23 +0200

    adjusting info style

--- src/components/CategoriesItem.jsx
+++ src/components/CategoriesItem.jsx
@@ -2,13 +2,26 @@ import styled from "styled-components"
 
 
 const Container = styled.div`
-    
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
 `
 const Image = styled.img`
-    
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `
 const Info = styled.div`
-    
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0%;
+    left: 0%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `
 const Title = styled.h1`
    
