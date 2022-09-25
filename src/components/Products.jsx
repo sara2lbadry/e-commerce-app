c83a1c9bbbb1959c0a597873652e4e6f10ebb7dd
Sara El Badry <sara2lbadry@gmail.com>
2022-09-25 15:08:53 +0200

    products & product component

---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import {popularProducts} from '../data'
+
+const Container = styled.div`
+    
+`
+function Products() {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Products
