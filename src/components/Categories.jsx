6327a6e60a0329dd122f4607ed41011273b9e3fd
Sara El Badry <sara2lbadry@gmail.com>
2022-09-25 14:46:12 +0200

    categories & categoryItem component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import {categories} from '../data'
+const Container = styled.div`
+    
+`
+function Categories() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Categories
---
+++ src/components/CategoriesItem.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    
+`
+function CategoriesItem() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default CategoriesItem
