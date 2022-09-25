3cadbb25d476bf5abf6c0137ca7bcc7e40e1db9c
Sara El Badry <sara2lbadry@gmail.com>
2022-09-25 15:32:31 +0200

    product list design

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,19 +2,48 @@ import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@m
 import styled from "styled-components"
 
 const Container = styled.div`
-    
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
 `
 const Circle = styled.div`
-    
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
 `
-const Image = styled.div`
-    
+const Image = styled.img`
+    height: 75%;
+    z-index: 2;
 `
 const Info = styled.div`
-    
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    background-color: gray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 3;
 `
 const Icon = styled.div`
-    
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: whitesmoke;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
 `
 function Product({item}) {
   return (
