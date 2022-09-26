5dba32998254a0f1c8b54942f6201bd6b73b7f06
Sara El Badry <sara2lbadry@gmail.com>
2022-09-26 23:23:56 +0200

    creating footer cpomponent

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    
+`
+function Footer() {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Footer
