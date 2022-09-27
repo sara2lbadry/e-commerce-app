2392b3197385e4fc6b99fbafa1d6f46dde0cf673
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 16:28:17 +0200

    socials icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,42 @@ import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material"
 import styled from "styled-components"
 
 const Container = styled.div`
-    
+    display: flex;
 `
 const Logo = styled.h1`
     
 `
 const Desc = styled.p`
-    
+    margin: 20px 0px;
 `
 const SocialContainer = styled.div`
-    
+    display: flex;
 `
 const SocialIcon = styled.div`
-    
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: whitesmoke;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
 `
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 2px;
 `
 const Center = styled.div`
     flex: 1;
+    padding: 20px;
 `
+
 const Right = styled.div`
     flex: 1;
+    padding: 20px;
 `
 function Footer() {
   return (
@@ -36,16 +50,16 @@ function Footer() {
                 fugiat, nam enim tenetur assumenda!
             </Desc>
             <SocialContainer>
-                <SocialIcon>
+                <SocialIcon color="3B5999">
                     <Facebook />
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="E4405F">
                     <Instagram />
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="55ACEE">
                     <Twitter />
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="E60023">
                     <Pinterest />
                 </SocialIcon>
             </SocialContainer>
