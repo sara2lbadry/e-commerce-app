8427669404434a43cb706759b03e3fbe8139f240
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 17:27:57 +0200

    creating single product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Navbar from "../components/Navbar"
+
+const Container = styled.div``
+function Product() {
+  return (
+    <Container>
+        <Navbar />
+        <Announcement />
+      
+    </Container>
+  )
+}
+
+export default Product
