61ff063a0cd1104dd859e568850a6ed8a7c6a31e
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 17:04:33 +0200

    ProductList jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,12 +1,30 @@
 import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Navbar from "../components/Navbar"
 
 const Container = styled.div`
+    
+`
+const Title = styled.h1`
+    
+`
+const FilterContainer = styled.div`
+    
+`
+const Filter = styled.div`
     
 `
 function ProductList() {
   return (
     <Container>
-        
+        <Navbar />
+        <Announcement />
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>filter</Filter>
+            <Filter>filter2</Filter>
+
+        </FilterContainer>
     </Container>
   )
 }
