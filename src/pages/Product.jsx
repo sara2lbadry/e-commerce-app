4a95163c63109ba8182d74b47faa84a72610ac88
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 17:49:01 +0200

    adding filters

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,24 +8,47 @@ const Container = styled.div`
 
 `
 const Wrapper = styled.div`
-    
+    padding: 50px;
+    display: flex;
 `
 const ImgContainer = styled.div`
-    
+    flex: 1;
 `
 const Image = styled.img`
-    
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
 `
 const InfoContainer = styled.div`
-    
+    flex: 1;
+    padding: 0 50px;
 `
 const Title = styled.h1`
-    
+    font-weight: 200;
 `
 const Desc = styled.p`
-    
+    margin: 20px 0;
 `
 const Price = styled.span`
+    font-weight: 100;
+    font-size: 20px;
+`
+const FilterContainer = styled.div`
+    
+`
+const Filter = styled.div`
+    
+`
+const FilterTitle = styled.div`
+    
+`
+const FilterColor = styled.div`
+    
+`
+const FilterSize = styled.div`
+    
+`
+const FilterSizeOption = styled.div`
     
 `
 function Product() {
@@ -46,7 +69,25 @@ function Product() {
                     tristique tortor pretium ut. Curabitur elit justo, consequat id
                     condimentum ac, volutpat ornare.
                 </Desc>
-                <Price></Price>
+                <Price>$ 30</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color = "black"/>
+                        <FilterColor color = "darkblue"/>
+                        <FilterColor color = "gray"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
             </InfoContainer>
         </Wrapper>
         <Newsletter />
