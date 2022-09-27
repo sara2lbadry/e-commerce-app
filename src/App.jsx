4a960fa307ad7645b6fc5c936d4a6850edda0a10
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 19:11:15 +0200

    login page jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-import Register from './pages/Register'
+import Login from './pages/Login'
 function App() {
   return (
     <div>
-        <Register />
+        <Login />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,38 @@ import styled from "styled-components"
 const Container = styled.div`
     
 `
+const Wrapper = styled.div`
+    
+`
+const Title = styled.h1`
+    
+`
+const Form = styled.form`
+    
+`
+const Input = styled.input`
+    
+`
+const Button = styled.button`
+    
+`
+
+const Link = styled.a`
+    
+`
 function Login() {
   return (
     <Container>
+         <Wrapper>
+            <Title>SIGN IN</Title>
+            <Form>
+                <Input placeholder="Username"/>
+                <Input placeholder="Password"/>
+                <Button>LOgIn</Button>
+                <Link>DON'T REMEMBER PASSWORD?</Link>
+                <Link>CREATE A NEW ACCOUNET</Link>
+            </Form>
+        </Wrapper>
       
     </Container>
   )
