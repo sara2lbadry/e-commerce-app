f2b5af4d8507c02400bff5e941b9a3992ca06abd
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 16:52:23 +0200

    moving pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import React from 'react'
-import Announcement from './components/Announcement'
-import Categories from './components/Categories'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Newsletter from './components/Newsletter'
-import Products from './components/Products'
-import Slider from './components/Slider'
+
+import Home from './pages/Home'
 function App() {
   return (
     <div>
-        <Announcement />
-        <Navbar />
-        <Slider />
-        <Categories />
-        <Products />
-        <Newsletter />
-        <Footer />
-
+        
+        <Home />
     </div>
   )
 }
