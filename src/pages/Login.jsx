fa2c95a1c1d10bc21d1c08991731bcf0d76e0434
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 19:18:13 +0200

    login page design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,56 @@
 import styled from "styled-components"
 
 const Container = styled.div`
-    
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+        center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 const Wrapper = styled.div`
-    
+    width: 25%;
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
 `
 const Title = styled.h1`
-    
+    font-size: 24px;
+    font-weight: 300;
 `
 const Form = styled.form`
-    
+    display: flex;
+    flex-direction: column;
 `
 const Input = styled.input`
-    
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0;
+    padding: 10px;
 `
 const Button = styled.button`
+    width: 40%;
+    border: none;
+    border-radius: 10px;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
     
-`
+`    
 
 const Link = styled.a`
-    
+    margin: 5px 0;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
 `
 function Login() {
   return (
@@ -30,7 +60,7 @@ function Login() {
             <Form>
                 <Input placeholder="Username"/>
                 <Input placeholder="Password"/>
-                <Button>LOgIn</Button>
+                <Button>LOGIN</Button>
                 <Link>DON'T REMEMBER PASSWORD?</Link>
                 <Link>CREATE A NEW ACCOUNET</Link>
             </Form>
