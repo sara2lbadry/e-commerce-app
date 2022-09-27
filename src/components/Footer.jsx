0e64a1bef1e802ea4828b791e1f2122b475b87d5
Sara El Badry <sara2lbadry@gmail.com>
2022-09-27 16:48:58 +0200

    footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material"
+import { Facebook, Instagram, LocationOn, MailOutline, Phone, Pinterest, Twitter } from "@mui/icons-material"
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -34,11 +34,34 @@ const Center = styled.div`
     flex: 1;
     padding: 20px;
 `
-
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
 const Right = styled.div`
     flex: 1;
     padding: 20px;
 `
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Payment = styled.img`
+    width: 50%;
+`
+
 function Footer() {
   return (
     <Container>
@@ -65,8 +88,36 @@ function Footer() {
             </SocialContainer>
 
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Oeder Tracking</ListItem>
+                <ListItem>Whishlist</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem>
+                <LocationOn style={{marginRight: "10px"}}/>
+                622 lorm ipsum, South 38622
+            </ContactItem>
+            <ContactItem>
+                <Phone style={{marginRight: "10px"}}/>
+                +1 234 56 789
+            </ContactItem>
+            <ContactItem>
+                <MailOutline style={{marginRight: "10px"}}/>
+                contact@lune.dev
+            </ContactItem>
+            <Payment  src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+        </Right>
     </Container>
   )
 }
