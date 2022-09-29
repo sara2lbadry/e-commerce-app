f57cae10a371832adbd0856657800abc9b3dfc79
Sara El Badry <sara2lbadry@gmail.com>
2022-09-29 16:14:26 +0200

    top section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,11 +6,55 @@ import Footer from '../components/Footer'
 const Container = styled.div`
     
 `
+const Wrapper = styled.div`
+    padding: 20px;
+`
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props => props.type === "filled" && "none"};
+    background-color: ${props => props.type === "filled" ? "black" : "transparent"};
+    color: ${props => props.type === "filled" && "white"};
+
+`
+const TopTexts = styled.div`
+    
+`
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0 10px;
+`
+const Bottom = styled.div`
+    
+`
 function Cart() {
   return (
     <Container>
         <Navbar />
         <Announcement />
+        <Wrapper>
+            <Title>YOUR BAG</Title>
+            <Top>
+                <TopButton>CONTINUE SHOPPING</TopButton>
+                <TopTexts>
+                    <TopText>Shopping Bag (2)</TopText>
+                    <TopText>Your Wishlist</TopText>
+                </TopTexts>
+                <TopButton type = "filled">CHECKOUT NOW</TopButton>
+            </Top>
+            <Bottom></Bottom>
+        </Wrapper>
         <Footer />
       
     </Container>
