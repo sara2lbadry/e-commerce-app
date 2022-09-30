aae0c88e83c03877149e57219424d4bfeb329b6e
Sara El Badry <sara2lbadry@gmail.com>
2022-09-30 19:20:36 +0200

    categories responsive design

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components"
 import {categories} from '../data'
 import CategoriesItem from "./CategoriesItem"
+import {mobile} from "../responsive"
 
 const Container = styled.div`
     display: flex;
     padding: 20px;
     justify-content: space-between;
+
+    ${mobile({
+        padding:"0px",
+        flexDirection: "column"
+    })}
 `
 function Categories() {
   return (
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import styled from "styled-components"
 import {sliderItems} from '../data'
 
+import {mobile} from '../responsive'
+
 const Container = styled.div`
     width: 100%;
     height: 100vh;
@@ -10,6 +12,10 @@ const Container = styled.div`
     /* background-color: #CBD9CE; */
     position: relative;
     overflow: hidden;
+
+    ${mobile({
+        display:"none"
+    })}
 `
 const Arrow = styled.div`
     width: 50px;
