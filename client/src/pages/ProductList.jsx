362e735cac566f3d9e8a6b9022e0eb204b3a06fe
Sara El Badry <sara2lbadry@gmail.com>
2022-10-08 16:27:30 +0200

    filter products color&size

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,96 +1,103 @@
-import styled from "styled-components"
-import Announcement from "../components/Announcement"
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
-import Newsletter from "../components/Newsletter"
-import Products from "../components/Products"
-import { mobile } from "../responsive"
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import { mobile } from "../responsive";
 
-const Container = styled.div`
-    
-`
+const Container = styled.div``;
 const Title = styled.h1`
-    margin: 20px;
-`
+  margin: 20px;
+`;
 const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 const Filter = styled.div`
-    margin: 20px;
+  margin: 20px;
 
-    ${mobile({
-        margin:"0 20px",
-        display:"flex", 
-        flexDirection:"column"
-    })}
-`
+  ${mobile({
+    margin: "0 20px",
+    display: "flex",
+    flexDirection: "column",
+  })}
+`;
 const FilterText = styled.span`
-    font-size: 20px;
-    font-weight: 600;
-    margin-right: 20px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
 
-    ${mobile({
-        marginRight:"0px"
-    })}
-`
+  ${mobile({
+    marginRight: "0px",
+  })}
+`;
 
 const Select = styled.select`
-    padding: 8px;
-    margin-right: 20px;
+  padding: 8px;
+  margin-right: 20px;
 
-    ${mobile({
-        margin:"10px 0px"
-    })}
-`
-const Option = styled.option`
-    
-`
+  ${mobile({
+    margin: "10px 0px",
+  })}
+`;
+const Option = styled.option``;
 function ProductList() {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filter, setFilters] = useState({});
+
+  const handleFilters = (event) => {
+    const value = event.target.value;
+    setFilters({
+      ...filter,
+      [event.target.name]: value,
+    });
+  };
+
+  console.log(filter);
   return (
     <Container>
-        <Navbar />
-        <Announcement />
-        <Title>Products</Title>
-        <FilterContainer>
-            <Filter><FilterText>Filter Products:</FilterText>
-                <Select>
-                    <Option disabled selected>
-                        Color
-                    </Option>
-                    <Option>White</Option>
-                    <Option>Black</Option>
-                    <Option>Red</Option>
-                    <Option>Blue</Option>
-                    <Option>Yellow</Option>
-                    <Option>Green</Option>
-                </Select>
-                <Select>
-                    <Option disabled selected>
-                        Size
-                    </Option>
-                    <Option>XS</Option>
-                    <Option>S</Option>
-                    <Option>M</Option>
-                    <Option>L</Option>
-                    <Option>XL</Option>
-                </Select>
-            </Filter>
-            <Filter><FilterText>Sort Products:</FilterText>
-                <Select>
-                    <Option selected>
-                        Newest
-                    </Option>
-                    <Option>Price (asc)</Option>
-                    <Option>Price (desc)</Option>
-                </Select>
-            </Filter>
-        </FilterContainer>
-        <Products />
-        <Newsletter />
-        <Footer />
+      <Navbar />
+      <Announcement />
+      <Title>Products</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
     </Container>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
