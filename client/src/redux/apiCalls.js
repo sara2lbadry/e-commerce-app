e33ea71ad7eda4a57bb659e20f149efabe35e1a7
Sara El Badry <sara2lbadry@gmail.com>
2022-10-13 14:42:32 +0200

    make api request

---
+++ client/src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import { loginFailure, loginStart, loginSuccess } from "./userSlice";
+import { publicRequest } from "../requestMethods";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (error) {
+    dispatch(loginFailure);
+  }
+};
