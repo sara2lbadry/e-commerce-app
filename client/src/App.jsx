2bfa37823e91cddff383a2873ba0247cba466d2d
Sara El Badry <sara2lbadry@gmail.com>
2022-10-16 16:48:16 +0200

    navigating user to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,18 @@ import Home from "./pages/Home"
 import {
   BrowserRouter as Router,
   Route,
-  Routes
+  Routes,
+  Navigate
 } from "react-router-dom";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart"
 import Login from "./pages/Login"
 import Regsiter from "./pages/Register"
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector(state => state.user.currentUser)
   return (
     <Router>
       <Routes>
@@ -19,7 +22,7 @@ function App() {
         <Route path = "/products/:cat"  element = {<ProductList />}></Route>
         <Route path = "/product/:id" element = {<Product />}></Route>
         <Route path = "/cart"  element = {<Cart />}></Route>
-        <Route path = "/login" element = {<Login />}></Route>
+        <Route path = "/login" element = {user ? <Navigate to="/" /> : <Login />}></Route>
         <Route path = "/regsiter" element = {<Regsiter />}></Route>
       </Routes>
     </Router>
